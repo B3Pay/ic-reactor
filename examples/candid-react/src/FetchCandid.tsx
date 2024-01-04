437faab1f55715a9f572aaab70595d7d18e67aa9
b3hr4d <behradmusiclove@yahoo.com>
2024-01-04 20:36:59 +0300

    Refactor FetchCandid component to improve error handling and add scrollable container

--- examples/candid-react/src/FetchCandid.tsx
+++ examples/candid-react/src/FetchCandid.tsx
@@ -69,37 +69,39 @@ const FormFields: React.FC<ReActorMethodField<any>> = ({
   return (
     <div>
       <Form callHandler={call} functionName={functionName} {...rest} />
-      {error && (
-        <fieldset className="border p-2 my-2 text-red-500 border-red-500 rounded">
-          <legend className="font-semibold">Error</legend>
-          <span className="text-sm">
-            <div>{error.message}</div>
-          </span>
-        </fieldset>
-      )}
-      {loading && (
-        <fieldset className="border p-2 my-2 rounded">
-          <legend className="font-semibold">Loading</legend>
-          <span className="text-sm">Calling...</span>
-        </fieldset>
-      )}
-      {data ? (
-        <fieldset className="border p-2 my-2 rounded">
-          <legend className="font-semibold">Results</legend>
-          <span className="text-sm">
-            {!data ? (
-              <div>Calling...</div>
-            ) : (
-              JSON.stringify(
-                data,
-                (_, value) =>
-                  typeof value === "bigint" ? value.toString() : value,
-                2
-              )
-            )}
-          </span>
-        </fieldset>
-      ) : null}
+      <div className="overflow-auto">
+        {error && (
+          <fieldset className="border p-2 my-2 text-red-500 border-red-500 rounded">
+            <legend className="font-semibold">Error</legend>
+            <span className="text-sm">
+              <div>{error.message}</div>
+            </span>
+          </fieldset>
+        )}
+        {loading && (
+          <fieldset className="border p-2 my-2 rounded">
+            <legend className="font-semibold">Loading</legend>
+            <span className="text-sm">Calling...</span>
+          </fieldset>
+        )}
+        {data ? (
+          <fieldset className="border p-2 my-2 rounded">
+            <legend className="font-semibold">Results</legend>
+            <span className="text-sm">
+              {!data ? (
+                <div>Calling...</div>
+              ) : (
+                JSON.stringify(
+                  data,
+                  (_, value) =>
+                    typeof value === "bigint" ? value.toString() : value,
+                  2
+                )
+              )}
+            </span>
+          </fieldset>
+        ) : null}
+      </div>
     </div>
   )
 }
