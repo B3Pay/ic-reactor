72a226878a250410544d8163eba1a670496b6d3a
b3hr4d <behradmusiclove@yahoo.com>
2024-02-06 10:26:28 +0300

    Refactor actor hooks in getActorHooks function

--- packages/react/src/hooks/actor.ts
+++ packages/react/src/hooks/actor.ts
@@ -241,8 +241,8 @@ export const getActorHooks = <A>({
 
   return {
     initialize,
-    useQueryCall: useQueryCall as any,
-    useUpdateCall: useUpdateCall as any,
+    useQueryCall,
+    useUpdateCall,
     useMethodCall,
     useActorStore,
     // __Details
