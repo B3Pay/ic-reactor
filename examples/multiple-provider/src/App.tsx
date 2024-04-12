ad4650788ce0ba6d5d20e66302e0c60e785c3635
b3hr4d <behradmusiclove@yahoo.com>
2024-04-12 12:18:16 +0300

    Refactor imports and remove unused code in App.tsx

--- examples/multiple-provider/src/App.tsx
+++ examples/multiple-provider/src/App.tsx
@@ -1,13 +1,10 @@
 import { useUserPrincipal } from "@ic-reactor/react"
 import ICRC1Call from "./ICRC2Call"
-import { FunctionName } from "@ic-reactor/react/dist/types"
-import { ICRC2 } from "./declarations/icrc2"
 import Donation from "./Donation"
 import Login from "./Login"
 import { ICRC2Provider } from "./ICRC2Provider"
 import { ICDVProvider } from "./ICDVProvider"
 import ICDVToken from "./ICDVToken"
-import { ICDV } from "./declarations/icdv"
 
 interface AppProps {}
 
