ac69c31e7829c242cb4bdf85f49e3fd4334b8026
b3hr4d <behradmusiclove@yahoo.com>
2024-04-20 22:11:38 +0300

    Fix import for "./parser.d.ts" in types.ts

--- packages/core/src/classes/candid/types.ts
+++ packages/core/src/classes/candid/types.ts
@@ -1,6 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="./parser.d.ts" />
 import type { AgentManager } from "../agent"
 import type { IDL, HttpAgent } from "../../types"
-import "./parser.d.ts"
 
 export interface CandidAdapterParameters {
   agentManager?: AgentManager
