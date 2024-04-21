957e131c5d6436335bc70920be7fd23686d875bd
b3hr4d <behradmusiclove@yahoo.com>
2024-04-21 14:55:38 +0300

    Update import paths and dependencies in various files

--- packages/core/src/utils/helper.ts
+++ packages/core/src/utils/helper.ts
@@ -1,6 +1,7 @@
 import { hash } from "@dfinity/agent"
 import { DevtoolsOptions, devtools } from "zustand/middleware"
 import { createStore } from "zustand/vanilla"
+
 import type { BaseActor, IDL } from "@src/types"
 
 export function createStoreWithOptionalDevtools<T>(
--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -1,4 +1,12 @@
 export * from "./helper"
 export * from "./constants"
-export { IDL } from "@dfinity/candid"
-export { Principal } from "@dfinity/principal"
+
+// this is for the UMD build
+import * as candidExports from "@dfinity/candid"
+// Destructure specific exports you want to ignore
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const { concat, bufFromBufLike, uint8ToBuf, ...rest } = candidExports
+
+export { rest }
+export * from "@dfinity/principal"
+export * from "@dfinity/agent"
