573bf9ed13811c6d9e5ae2ad6f8aac79f01bfe31
b3hr4d <behradmusiclove@yahoo.com>
2026-02-16 10:10:20 +0300

    fix: set default value of withCanisterEnv to true in ClientManager

--- packages/core/src/client.ts
+++ packages/core/src/client.ts
@@ -76,7 +76,7 @@ export class ClientManager {
     port = 4943,
     withLocalEnv,
     withProcessEnv,
-    withCanisterEnv,
+    withCanisterEnv = true,
     agentOptions = {},
     queryClient,
     authClient,
